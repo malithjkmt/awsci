d676c42f60592238639b3a9c54c2d20cd974e364
Malith <malit.tilak@gmail.com>
2018-03-31 12:57:13 +0530

    configure circleCi

---
+++ handler.js
@@ -0,0 +1,11 @@
+const { generate } = require('unique-names-generator');
+
+exports.handler = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      name: generate(),
+    }),
+  };
+callback(null, response);
+};
